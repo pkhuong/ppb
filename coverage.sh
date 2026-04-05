bac2e7a1623759cd751694977df570c8fd87b0cd
Paul Khuong <pvk@pvk.ca>
2026-04-05 00:32:20 -0400

    coverage.sh: add support for EXTRA_FLAGS

--- coverage.sh
+++ coverage.sh
@@ -2,21 +2,22 @@
 set -eu
 
 COVDIR=build/coverage
+EXTRA_FLAGS="${EXTRA_FLAGS:-}"
 
 rm -rf "$COVDIR"
 mkdir -p "$COVDIR"
 
 # Build picoscope with coverage instrumentation (bypass ccache).
 # Compile objects into COVDIR so .gcno/.gcda land together.
-gcc -std=c2x -O0 -Iinclude/ -Wall -Wextra -Wpedantic -ggdb \
+gcc -std=c2x -O0 -Iinclude/ -Wall -Wextra -Wpedantic -ggdb $EXTRA_FLAGS \
     --coverage \
     -c -o "$COVDIR/ppb.o" src/ppb.c
-gcc -std=c2x -O0 -Iinclude/ -Wall -Wextra -Wpedantic -ggdb \
+gcc -std=c2x -O0 -Iinclude/ -Wall -Wextra -Wpedantic -ggdb $EXTRA_FLAGS \
     --coverage \
     -c -o "$COVDIR/picoscope.o" examples/picoscope.c
 gcc --coverage -o "$COVDIR/picoscope" "$COVDIR/picoscope.o" "$COVDIR/ppb.o"
 
-gcc -std=c2x -O0 -Iinclude/ -I. -Wall -Wextra -Wpedantic -ggdb \
+gcc -std=c2x -O0 -Iinclude/ -I. -Wall -Wextra -Wpedantic -ggdb $EXTRA_FLAGS \
     --coverage \
     -c -o "$COVDIR/test_ppb.o" tests/test_ppb.c
 gcc --coverage -o "$COVDIR/test_ppb" "$COVDIR/test_ppb.o" "$COVDIR/ppb.o"
