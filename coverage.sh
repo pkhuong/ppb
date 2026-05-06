84c116831de9e042a0c64f41c6ba45b36d0bdf59
Paul Khuong <pvk@pvk.ca>
2026-05-06 01:39:37 -0400

    .github/ci: run C++ unit tests on all platforms
    But don't gather coverage for the new code: too much stuff is
    tested constexpr.

--- coverage.sh
+++ coverage.sh
@@ -19,8 +19,15 @@ gcc -std=c2x -O0 -Iinclude/ -I. -Wall -Wextra -Wpedantic -ggdb $EXTRA_FLAGS \
     -c -o "$COVDIR/test_ppb.o" tests/test_ppb.c
 gcc --coverage -o "$COVDIR/test_ppb" "$COVDIR/test_ppb.o" "$COVDIR/ppb.o"
 
+# Build and link the C++ test harness against the same instrumented ppb.o
+# so .hpp headers get .gcno/.gcda alongside src/ppb.c.
+g++ -std=c++20 -O0 -Iinclude/ -I. -Wall -Wextra -Wpedantic -ggdb $EXTRA_FLAGS \
+    -c -o "$COVDIR/test_ppb_cpp.o" tests/test_ppb_cpp.cc
+g++ --coverage -o "$COVDIR/test_ppb_cpp" "$COVDIR/test_ppb_cpp.o" "$COVDIR/ppb.o"
+
 # Run the test suites.
 "$COVDIR/test_ppb"
+"$COVDIR/test_ppb_cpp"
 sh test_picoscope.sh "$COVDIR/picoscope"
 
 # Generate HTML report.
