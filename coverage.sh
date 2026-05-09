0a6c8e7ef946fc0b45a977b69f250e66e14c443a
Paul Khuong <pvk@pvk.ca>
2026-05-08 23:18:21 -0400

    coverage.sh: improve C++ support
    C++ needs optimisation, otherwise we get weird phantom code.  And
    with our template abuse, we have to tell gcovr to merge instantiations.
    
    And opt out of gcovr in CI: we only need the gcda, not the report.

--- coverage.sh
+++ coverage.sh
@@ -21,7 +21,7 @@ gcc --coverage -o "$COVDIR/test_ppb" "$COVDIR/test_ppb.o" "$COVDIR/ppb.o"
 
 # Build and link the C++ test harness against the same instrumented ppb.o
 # so .hpp headers get .gcno/.gcda alongside src/ppb.c.
-g++ -std=c++20 -O0 -Iinclude/ -I. -Wall -Wextra -Wpedantic -ggdb $EXTRA_FLAGS \
+g++ -std=c++20 -O2 -Iinclude/ -I. -Wall -Wextra -Wpedantic -ggdb $EXTRA_FLAGS \
     -c -o "$COVDIR/test_ppb_cpp.o" tests/test_ppb_cpp.cc
 g++ --coverage -o "$COVDIR/test_ppb_cpp" "$COVDIR/test_ppb_cpp.o" "$COVDIR/ppb.o"
 
@@ -30,8 +30,11 @@ g++ --coverage -o "$COVDIR/test_ppb_cpp" "$COVDIR/test_ppb_cpp.o" "$COVDIR/ppb.o
 "$COVDIR/test_ppb_cpp"
 sh test_picoscope.sh "$COVDIR/picoscope"
 
-# Generate HTML report.
-gcovr --root . --object-directory "$COVDIR" \
-    --html-details "$COVDIR/report/index.html"
+if [ -z "${GCDA_ONLY:-}" ]
+then
+    # Generate HTML report.
+    gcovr --root . --object-directory "$COVDIR" --merge-lines \
+        --html-details "$COVDIR/report/index.html"
 
-printf "\nCoverage report: %s/report/index.html\n" "$COVDIR"
+    printf "\nCoverage report: %s/report/index.html\n" "$COVDIR"
+fi
